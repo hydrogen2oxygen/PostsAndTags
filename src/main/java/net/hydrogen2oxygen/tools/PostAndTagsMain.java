4aa9e1d1511f3f7d2e76e0c6463427df9f0a20f5
hydrogen2oxygen <luxuspeter@googlemail.com>
2016-06-10 15:20:47 +0200

    Tags inside Template

--- src/main/java/net/hydrogen2oxygen/tools/PostAndTagsMain.java
+++ src/main/java/net/hydrogen2oxygen/tools/PostAndTagsMain.java
@@ -12,122 +12,132 @@ import org.apache.commons.io.FileUtils;
 
 public class PostAndTagsMain implements IConstants {
 
-	private static final String UTF_8 = "UTF-8";
-	private Properties properties = new Properties();
-	private DataCollector dataCollector = new DataCollector();
+   private static final String UTF_8 = "UTF-8";
 
-	public static void main(String[] args) throws Exception {
+   private Properties properties = new Properties();
 
-		if (args.length == 0) {
-			System.out.println("At least provide a properties file as first parameter!");
-			System.exit(0);
+   private DataCollector dataCollector = new DataCollector();
 
-		}
+   public static void main(String[] args) throws Exception {
 
-		String propertiesFile = args[0];
-		IConstants postsAndTags = new PostAndTagsMain(propertiesFile);
+      if (args.length == 0) {
+         System.out.println("At least provide a properties file as first parameter!");
+         System.exit(0);
 
-	}
+      }
 
-	private PostAndTagsMain(String propertiesFile) throws Exception {
+      String propertiesFile = args[0];
+      IConstants postsAndTags = new PostAndTagsMain(propertiesFile);
 
-		// Init configurations
-		initProperties(propertiesFile);
+   }
 
-		// First collect all files inside a source folder
-		File sourceDirectory = new File(properties.getProperty(IConstants.SOURCE_DIRECTORY));
-		String targetDirectory = properties.getProperty(IConstants.TARGET_DIRECTORY);
+   private PostAndTagsMain(String propertiesFile) throws Exception {
 
-		addFilesFromFolder(sourceDirectory);
+      // Init configurations
+      initProperties(propertiesFile);
 
-		File templateFile = new File(properties.getProperty(IConstants.TEMPLATE_FILE));
-		String template = FileUtils.readFileToString(templateFile, UTF_8);
+      // First collect all files inside a source folder
+      File sourceDirectory = new File(properties.getProperty(IConstants.SOURCE_DIRECTORY));
+      String targetDirectory = properties.getProperty(IConstants.TARGET_DIRECTORY);
 
-		// Extract the tags and store them in memory (provide enough for your
-		// instance with VM parameters!)
-		for (File file : dataCollector.getListOfFiles()) {
+      addFilesFromFolder(sourceDirectory);
 
-			String content = FileUtils.readFileToString(file, UTF_8);
-			Pattern p = Pattern.compile("\\[[A-z]+\\]");
-			Matcher m = p.matcher(content);
+      File templateFile = new File(properties.getProperty(IConstants.TEMPLATE_FILE));
+      String template = FileUtils.readFileToString(templateFile, UTF_8);
 
-			while (m.find()) {
-				String tagWithSquareBrackets = content.substring(m.start(), m.end());
-				String tag = tagWithSquareBrackets.toLowerCase().replaceAll("\\[", "").replaceAll("\\]", "").trim();
+      File tagTtemplateFile = new File(properties.getProperty(IConstants.TAG_TEMPLATE_FILE));
+      String tagTemplate = FileUtils.readFileToString(tagTtemplateFile, UTF_8);
 
-				dataCollector.addTag(tag);
-			}
+      // Extract the tags and store them in memory (provide enough for your
+      // instance with VM parameters!)
+      for (File file : dataCollector.getListOfFiles()) {
 
-		}
+         String content = FileUtils.readFileToString(file, UTF_8);
+         Pattern p = Pattern.compile("\\[[A-z]+\\]");
+         Matcher m = p.matcher(content);
 
-		// Start generating the target html files
-		for (File file : dataCollector.getListOfFiles()) {
+         while (m.find()) {
+            String tagWithSquareBrackets = content.substring(m.start(), m.end());
+            String tag = tagWithSquareBrackets.toLowerCase().replaceAll("\\[", "").replaceAll("\\]", "").trim();
 
-			String content = FileUtils.readFileToString(file, UTF_8);
+            dataCollector.addTag(tag);
+         }
 
-			for (String tag : dataCollector.getListOfTags()) {
-				content = content.replaceAll("\\[" + tag + "]", generateLink(tag));
-			}
+      }
 
-			content = template.replaceAll("###CONTENT###", content);
+      // Start generating the target html files
+      for (File file : dataCollector.getListOfFiles()) {
 
-			File targetFile = new File(targetDirectory + "/" + file.getName());
-			FileUtils.writeStringToFile(targetFile, content, UTF_8);
-		}
+         String content = FileUtils.readFileToString(file, UTF_8);
 
-		// Now in a second run generate the tags html
-		for (String tag : dataCollector.getListOfTags()) {
+         for (String tag : dataCollector.getListOfTags()) {
+            content = content.replaceAll("\\[" + tag + "]", generateLink(tag));
+         }
 
-			File tagFile = new File(targetDirectory + "/" + tag + ".html");
+         content = template.replaceAll("###CONTENT###", content);
 
-			if (!tagFile.exists()) {
-				File targetFile = new File(targetDirectory + "/" + tag + ".html");
-				FileUtils.writeStringToFile(targetFile, generateTagContent(tag), UTF_8);
-			}
-		}
+         File targetFile = new File(targetDirectory + "/" + file.getName());
+         FileUtils.writeStringToFile(targetFile, content, UTF_8);
+      }
 
-		// Generate a protocol also as a html file inside the folder for
-		// protocol
-	}
+      // Now in a second run generate the tags html
+      for (String tag : dataCollector.getListOfTags()) {
 
-	private String generateTagContent(String tag) {
-		// TODO Auto-generated method stub
-		return tag;
-	}
+         File tagFile = new File(targetDirectory + "/" + tag + ".html");
 
-	private String generateLink(String tag) {
+         if (!tagFile.exists()) {
 
-		String link = "<a href=\"" + tag + ".html\">" + tag + "</a>";
-		return link;
-	}
+            String content = generateTagContent(tag, tagTemplate, template);
+            File targetFile = new File(targetDirectory + "/" + tag + ".html");
+            FileUtils.writeStringToFile(targetFile, content, UTF_8);
+         }
+      }
 
-	private void addFilesFromFolder(File folder) {
-		for (File file : folder.listFiles()) {
+      // Generate a protocol also as a html file inside the folder for
+      // protocol
+   }
 
-			if (file.isFile()) {
+   private String generateTagContent(String tag, String tagTemplate, String template) {
 
-				dataCollector.addFile(file);
-			}
+      String tagContent = tagTemplate.replaceAll("###TAG###", tag.toUpperCase());
+      String content = template.replaceAll("###CONTENT###", tagContent).replaceAll("###TITLE###", tag.toUpperCase());
+      return content;
+   }
 
-			// Recursively move into sub folders
-			if (file.isDirectory()) {
-				addFilesFromFolder(file);
-			}
-		}
-	}
+   private String generateLink(String tag) {
 
-	private void initProperties(String propertiesFile) throws FileNotFoundException, IOException {
-		FileInputStream in = new FileInputStream(propertiesFile);
-		properties.load(in);
-	}
+      String link = "<a href=\"" + tag + ".html\">" + tag + "</a>";
+      return link;
+   }
 
-	/**
-	 * This method will create a folder structure with default templates for
-	 * your convenience
-	 * 
-	 * @param projectName
-	 */
-	private void createNewProject(String projectName) {
+   private void addFilesFromFolder(File folder) {
 
-	}
+      for (File file : folder.listFiles()) {
+
+         if (file.isFile()) {
+
+            dataCollector.addFile(file);
+         }
+
+         // Recursively move into sub folders
+         if (file.isDirectory()) {
+            addFilesFromFolder(file);
+         }
+      }
+   }
+
+   private void initProperties(String propertiesFile) throws FileNotFoundException, IOException {
+
+      FileInputStream in = new FileInputStream(propertiesFile);
+      properties.load(in);
+   }
+
+   /**
+    * This method will create a folder structure with default templates for your convenience
+    *
+    * @param projectName
+    */
+   private void createNewProject(String projectName) {
+
+   }
 }
